a0723c6e952e6170d5a91d216f430c07e9d01f04
Divy <divy.jain@bombaysoftwares.com>
2023-01-30 18:13:20 +0530

    added delete button in todoitem

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -5,6 +5,7 @@ export const TodoItem = (props) => {
     <div>
       <h4>{props.todo.title}</h4>
       <p>{props.todo.desc}</p>
+      <button className='btn btn-danger'>Delete</button>
     </div>
   )
 }
