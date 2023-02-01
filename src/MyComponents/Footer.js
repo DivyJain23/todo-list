62eb9ca4fe7b206f86d027ee9139c4a46ee76970
Divy <divy.jain@bombaysoftwares.com>
2023-02-01 16:44:46 +0530

    designed footer component

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 export const Footer = () => {
+
+  let footerStyles = {
+    position : "absolute",
+    top : "100vh",
+    width : "100%"
+  }
+
   return (
-    <div>Footer Works</div>
+    <div className="bg-dark text-light py-4" style={footerStyles}>
+      <p className="text-center">
+        Copyright &copy; MyTodoList.com
+      </p>
+    </div>
   )
 }
