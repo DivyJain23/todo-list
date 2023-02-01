82aa35b4a4dff1eb92a854b0c63a0d63aa423d75
Divy <divy.jain@bombaysoftwares.com>
2023-02-01 16:35:04 +0530

    -added a placeholder component to show a message in case of empty todo list

---
+++ src/MyComponents/Common/Emptyplaceholder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Emptyplaceholder = (props) => {
+  return (
+    <div>
+        <h3>{props.desc}</h3>
+    </div>
+  )
+}
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Emptyplaceholder } from './Common/Emptyplaceholder';
 import { TodoItem } from './TodoItem';
+
 export const Todos = (props) => {
   return (
     <div className='container'>
       <h3 className=' text-center my-3'>Todos List</h3>
-      {props.todos.map((todo) => {
-        return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}></TodoItem>
-      })}
+      {props.todos.length === 0 ? <Emptyplaceholder desc="No Todos Found" /> :
+        props.todos.map((todo) => {
+          return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}></TodoItem>
+        })}
     </div>
   )
 }
